fc093132f7cbe0ab38be1d0b9fa1b38f4c5cd5a9
Crypt0nic0 <n.greneche45@gmail.com>
2023-03-09 23:10:14 +0100

    updated

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ lignes... */
 // Kamel Case
 let maSuperVariable = "Hello";
 
+// ** Les variables ** //
+
+// var = vieux JavaScript, n'est plus utilisé aujourd'hui
+var unTexte = "voici un texte";
+
+// const = constante = type de variable qui ne peut être modifié
+const prenom = "justine";
+
+// let = la variable peut être modifiée
+let unChiffre = 24;
+
+
+console.log(unChiffre);
